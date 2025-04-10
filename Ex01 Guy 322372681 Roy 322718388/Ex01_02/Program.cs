8dd5b851b8abe0dfa64a4c7ea8df6274ee380ae3
GuyFirst <guyfirst19@gmail.com>
2025-04-10 05:18:50 +0300

    hehe recursive tree goes brrr

---
+++ Ex01 Guy 322372681 Roy 322718388/Ex01_02/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace Ex01_02
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            RecursiveTree.PrintTree();
+        }
+    }
+}
---
+++ Ex01 Guy 322372681 Roy 322718388/Ex01_02/RecursiveTree.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex01_02
+{
+    public class RecursiveTree
+    {
+       
+        public static void PrintTree()
+        {
+            printTreeRecursive(1, 'A', 1, 11);
+        }
+
+        private static void printTreeRecursive(int i_numToStartPrintFrom, char i_charToPrint, int i_numOfNumbers, int i_numOfSpaces)
+        {
+            if (i_charToPrint == 'A')
+            {
+                Console.WriteLine("A           1");
+                printTreeRecursive(i_numToStartPrintFrom + 1, (char)((int)i_charToPrint + 1), i_numOfNumbers + 2, i_numOfSpaces - 2);
+            }
+            else
+            {
+                if (i_charToPrint == 'F')
+                {
+                    Console.WriteLine("F          |8|");
+                    Console.WriteLine("G          |8|");
+                }
+                else
+                {
+                    Console.Write(i_charToPrint);
+                    for (int i = 0; i < i_numOfSpaces; i++)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int i = 0; i < i_numOfNumbers; i++)
+                    {
+                        Console.Write(i_numToStartPrintFrom + " ");
+                        i_numToStartPrintFrom = (i_numToStartPrintFrom + 1) % 10;
+                        if (i_numToStartPrintFrom == 0)
+                        {
+                            i_numToStartPrintFrom = 1;
+                        }
+                    }
+                    Console.Write('\n');
+                    printTreeRecursive(i_numToStartPrintFrom, (char)((int)i_charToPrint + 1), i_numOfNumbers + 2, i_numOfSpaces - 2);
+                }
+                   
+            }
+            return;
+        }
+    }
+}
