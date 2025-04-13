95aae74b61c2d595321d2f98458fedb6c09d8656
ROYS-WIN-PC <royda2@mta.ac.il>
2025-04-13 21:58:53 +0300

    fixing lower-cased arguments to fit Gayro's conventions

--- Ex01 Guy 322372681 Roy 322718388/Ex01_05/ConsoleHandler.cs
+++ Ex01 Guy 322372681 Roy 322718388/Ex01_05/ConsoleHandler.cs
@@ -27,16 +27,16 @@ Please enter a whole number with 8 digits: ");
             }
             return input;
         }
-        public static void ShowStatistics(NumberWithStatistics i_number)
+        public static void ShowStatistics(NumberWithStatistics i_Number)
         {
-            int firstDigit = (int)(char.GetNumericValue(i_number.GetNumberAsString()[0]));
-            int numOfDigitSmallerThanFirstDigit = i_number.GetNumOfDigitsSmallerThanFirstDigit();
-            int numOfDigitsDivisibleBy3 = i_number.GetNumOfDigitsDivisibleBy3();
-            string digitsDivisibleBy3 = i_number.GetDigitsDivisibleBy3().ToString();
-            string digits = i_number.GetDigitsSmallerThanFirstDigit().ToString();
-            int differenceBetweenLargestDigitToSmallestDigit = i_number.GetDifferenceBetweenLargestAndSmallestDigits();
-            char mostCommonDigitInNumber = i_number.GetMostFrequentDigit();
-            int frequencyOfMostCommonDigit = i_number.GetFrequencyOfMostFrequentDigit();
+            int firstDigit = (int)(char.GetNumericValue(i_Number.GetNumberAsString()[0]));
+            int numOfDigitSmallerThanFirstDigit = i_Number.GetNumOfDigitsSmallerThanFirstDigit();
+            int numOfDigitsDivisibleBy3 = i_Number.GetNumOfDigitsDivisibleBy3();
+            string digitsDivisibleBy3 = i_Number.GetDigitsDivisibleBy3().ToString();
+            string digits = i_Number.GetDigitsSmallerThanFirstDigit().ToString();
+            int differenceBetweenLargestDigitToSmallestDigit = i_Number.GetDifferenceBetweenLargestAndSmallestDigits();
+            char mostCommonDigitInNumber = i_Number.GetMostFrequentDigit();
+            int frequencyOfMostCommonDigit = i_Number.GetFrequencyOfMostFrequentDigit();
 
             Console.WriteLine();
             showNumberOfDigitsSmallerThenFirstDigit(firstDigit, numOfDigitSmallerThanFirstDigit, digits);   
@@ -44,33 +44,33 @@ Please enter a whole number with 8 digits: ");
             showDifferenceBetweenLargestDigitToSmallestDigit(differenceBetweenLargestDigitToSmallestDigit);
             showMostCommonDigitAndNumberOfFrequency(mostCommonDigitInNumber, frequencyOfMostCommonDigit);
         }
-        private static void showNumberOfDigitsSmallerThenFirstDigit(int i_firstDigit, int i_numOfDigitSmallerThanFirstDigit, string i_digits)
+        private static void showNumberOfDigitsSmallerThenFirstDigit(int i_FirstDigit, int i_NumOfDigitSmallerThanFirstDigit, string i_Digits)
         {
             Console.Write(string.Format(
 @"1.The first (most left) digit is {0}. There are {1} digits in the provided number 
-which are smaller then {0}", i_firstDigit, i_numOfDigitSmallerThanFirstDigit));
-            if (i_digits.Length > 0)
+which are smaller then {0}", i_FirstDigit, i_NumOfDigitSmallerThanFirstDigit));
+            if (i_Digits.Length > 0)
             {
-                Console.WriteLine(string.Format(", which are {0}", i_digits));
+                Console.WriteLine(string.Format(", which are {0}", i_Digits));
             }
             Console.WriteLine();
         }
-        private static void showHowManyDigitsDivisibleBy3(int i_numOfDigitsDivisibleBy3, string i_digitsDivisibleBy3)
+        private static void showHowManyDigitsDivisibleBy3(int i_NumOfDigitsDivisibleBy3, string i_DigitsDivisibleBy3)
         { 
             Console.WriteLine(string.Format(
-@"2.There are {0} numbers which are divisible by 3, which are {1}", i_numOfDigitsDivisibleBy3, i_digitsDivisibleBy3));
+@"2.There are {0} numbers which are divisible by 3, which are {1}", i_NumOfDigitsDivisibleBy3, i_DigitsDivisibleBy3));
             Console.WriteLine();
         }
-        private static void showDifferenceBetweenLargestDigitToSmallestDigit(int i_differenceBetweenLargestDigitToSmallestDigit)
+        private static void showDifferenceBetweenLargestDigitToSmallestDigit(int i_DifferenceBetweenLargestDigitToSmallestDigit)
         {
             Console.WriteLine(string.Format(
-@"3.The difference between the largest digit to the smallest digit is {0}.", i_differenceBetweenLargestDigitToSmallestDigit));
+@"3.The difference between the largest digit to the smallest digit is {0}.", i_DifferenceBetweenLargestDigitToSmallestDigit));
             Console.WriteLine();
         }
-        private static void showMostCommonDigitAndNumberOfFrequency(char i_mostCommonDigit, int i_frequency)
+        private static void showMostCommonDigitAndNumberOfFrequency(char i_MostCommonDigit, int i_Frequency)
         {
             Console.WriteLine(string.Format(
-@"4.The most common digit in the provided number is {0}, which appeared {1} times.", i_mostCommonDigit, i_frequency));
+@"4.The most common digit in the provided number is {0}, which appeared {1} times.", i_MostCommonDigit, i_Frequency));
             Console.WriteLine();
         }
     }
