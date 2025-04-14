83f410342fe48d4a5fc3ce7ee51120e87b92badf
GuyFirst <guyfirst19@gmail.com>
2025-04-14 14:22:32 +0300

    fixed spaces in ex01_02

--- Ex01 Guy 322372681 Roy 322718388/Ex01_02/RecursiveTree.cs
+++ Ex01 Guy 322372681 Roy 322718388/Ex01_02/RecursiveTree.cs
@@ -8,43 +8,45 @@ namespace Ex01_02
         {
             printTreeRecursive(ref i_NumOfRows, 0, 'A', 1, i_NumOfRows);
         }
-        private static void printTreeRecursive(ref int i_NumberOfLines, int i_CurrentLineNumber, char i_lastPrintedChar, int i_lastPrintedNumber, int i_MiddleTreePosition)
+        private static void printTreeRecursive(ref int i_NumberOfLines, int i_CurrentLineNumber, char i_LastPrintedChar, int i_LastPrintedNumber, int i_MiddleTreePosition)
         {
-            if (i_lastPrintedChar == 'Z')
+            if (i_LastPrintedChar == 'Z')
             {
-                i_lastPrintedChar = 'A';
+                i_LastPrintedChar = 'A';
             }
-            if (i_lastPrintedNumber == 10)
+
+            if (i_LastPrintedNumber == 10)
             {
-                i_lastPrintedNumber = 1;
+                i_LastPrintedNumber = 1;
             }
+
             if (i_CurrentLineNumber + 1 == i_NumberOfLines - 1)
             {
-                printTrunkOfTree(i_MiddleTreePosition, i_lastPrintedNumber, i_lastPrintedChar);
+                printTrunkOfTree(i_MiddleTreePosition, i_LastPrintedNumber, i_LastPrintedChar);
             }
             else
             {
-                printRowInTree(i_lastPrintedNumber, i_lastPrintedChar, i_NumberOfLines, i_CurrentLineNumber);
-                printTreeRecursive(ref i_NumberOfLines, ++i_CurrentLineNumber, ++i_lastPrintedChar, i_lastPrintedNumber, i_MiddleTreePosition);
+                printRowInTree(i_LastPrintedNumber, i_LastPrintedChar, i_NumberOfLines, i_CurrentLineNumber);
+                printTreeRecursive(ref i_NumberOfLines, ++i_CurrentLineNumber, ++i_LastPrintedChar, i_LastPrintedNumber, i_MiddleTreePosition);
             }
         }
-        private static void printRowInTree(int i_lastPrintedNumber, char i_lastPrintedChar, int i_NumberOfLines, int i_CurrentLineNumber)
+        private static void printRowInTree(int i_LastPrintedNumber, char i_LastPrintedChar, int i_NumberOfLines, int i_CurrentLineNumber)
         {
-            Console.Write("{0}", i_lastPrintedChar);
+            Console.Write("{0}", i_LastPrintedChar);
             printSpaces(i_NumberOfLines - i_CurrentLineNumber - 1);
-            printNumbersInRow((i_CurrentLineNumber * 2) + 1, ref i_lastPrintedNumber);
+            printNumbersInRow((i_CurrentLineNumber * 2) + 1, ref i_LastPrintedNumber);
             Console.WriteLine();
         }
-        private static void printTrunkOfTree(int i_MiddleTreePosition, int i_lastPrintedNumber, char i_lastPrintedChar)
+        private static void printTrunkOfTree(int i_MiddleTreePosition, int i_LastPrintedNumber, char i_LastPrintedChar)
         {
             i_MiddleTreePosition -= 2;
-            Console.Write("{0} ", i_lastPrintedChar);
+            Console.Write("{0} ", i_LastPrintedChar);
             printSpaces(i_MiddleTreePosition);
-            Console.WriteLine("|{0}|", i_lastPrintedNumber);
+            Console.WriteLine("|{0}|", i_LastPrintedNumber);
             Console.WriteLine();
-            Console.Write("{0} ", ++i_lastPrintedChar);
+            Console.Write("{0} ", ++i_LastPrintedChar);
             printSpaces(i_MiddleTreePosition);
-            Console.WriteLine("|{0}|", i_lastPrintedNumber);
+            Console.WriteLine("|{0}|", i_LastPrintedNumber);
         }
         private static void printSpaces(int i_Count)
         {
@@ -52,6 +54,7 @@ namespace Ex01_02
             {
                 return;
             }
+
             Console.Write("  ");
             printSpaces(i_Count - 1);
         }
@@ -67,6 +70,7 @@ namespace Ex01_02
                     i_NumberToStartFrom = 1;
                 }
             }
+
             Console.WriteLine();
         }
     }
